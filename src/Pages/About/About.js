c2c542fd50766af2ede70fe3d7b43bb0ff1f42ce
Farhan-Abid <shahabid628@gmail.com>
2022-04-19 18:01:18 +0600

    added infos in know me section

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -5,7 +5,7 @@ const About = () => {
     <div className="section container">
       <h2 className="text-center my-3">About Me</h2>
       <p>
-        
+        I am <b>Shah Farhan Abid</b>. I am an upcoming computar science engineer who is passionate about Web Developing. That's why i am willing to establish my career as an full stack web developer. I have already learned and familiar myself with Html,css,Javascript,React,Firebase and in the upcoming days,i will also gain knowledge in MongoDb and so on. I also did work on 10+ projects so far based on these learning which was enjoyable for me. I also have interests on sports and i am a huge football fan. I am not less than a hardworking and a team member type of person who always try to complete given any task or project with my sincerety,hardworking ability and learning habbit. 
       </p>
     </div>
   );
